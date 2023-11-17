838e25486df42e7210249ea946541f15884cb8d9
GentritUkelli <112003299+GentritUkelli@users.noreply.github.com>
2023-11-17 21:15:12 +0100

    no message

--- CarShop/script.js
+++ CarShop/script.js
@@ -1,250 +1,247 @@
-$(document).ready(function () {
-  let  cars = [
-    {
-      id: "1",
-      make: "Mercedes Benz",
-      model: "C-Class (W205:2015)",
-      price: 45000,
-      image: "photos/C-class.jpg",
-    },
-    {
-      id: "2",
-      make: "BMW",
-      model: "3 Series (G28)",
-      price: 42000,
-      image: "photos/BMW 3 series.jpeg",
-    },
-    {
-      id: "3",
-      make: "Volkswagen (VW)",
-      model: "Golf 8",
-      price: 28000,
-      image: "photos/VW Golf 8.jpg",
-    },
-    {
-      id: "4",
-      make: "Ford",
-      model: "Fusion",
-      price: 32000,
-      image: "photos/Ford Fusion.jpg",
-    },
-    {
-      id: "5",
-      make: "Toyota",
-      model: "Camry",
-      price: 35000,
-      image: "photos/Toyota Camry.png",
-    },
-    {
-      id: "6",
-      make: "Tesla",
-      model: "Model 3",
-      price: 52000,
-      image: "photos/Tesla Model 3.jpg",
-    },
-    {
-      id: "7",
-      make: "Nissan",
-      model: "Leaf",
-      price: 29000,
-      image: "photos/Nissan Leaf.jpg",
-    },
-    {
-      id: "8",
-      make: "Audi",
-      model: "A4",
-      price: 47000,
-      image: "photos/Audi-A4.jpg",
-    },
-    {
-      id: "9",
-      make: "Mercedes Benz",
-      model: "E-Class",
-      price: 55000,
-      image: "photos/Mercedes E-Class.jpg",
-    },
-    {
-      id: "10",
-      make: "Chevrolet",
-      model: "Bolt",
-      price: 32000,
-      image: "photos/Chevrolet-Bolt.jpg",
-    },
-    {
-      id: "11",
-      make: "BMW",
-      model: "i3",
-      price: 49000,
-      image: "photos/BMW i3.jpg",
-    },
-  ];
- 
-  function addCar(make, model, price, image) {
-      const newCar = {
-        id: (cars.length + 1).toString(),
-        make: make,
-        model: model,
-        price: price,
-        image: image,
-      };
-  
-      cars.push(newCar);
-      displayAllCars();
-    }
-  
-    $('#car-form').submit(function (event) {
-      event.preventDefault();
-  
-      var make = $('#make-input').val();
-      var model = $('#model-input').val();
-      var price = $('#price-input').val();
-      var imageInput = $('#image-input')[0];
-  
-      if (!make || !model || !price || !imageInput.files[0]) {
-        alert('Please fill in all fields.');
-        return;
-      }
-  
-      var imageFile = imageInput.files[0];
-      var reader = new FileReader();
-  
-      reader.onload = function (e) {
-        var image = e.target.result;
-        addCar(make, model, price, image);
-        $('#car-form')[0].reset();
-      };
-  
-      reader.readAsDataURL(imageFile);
-    });
-  
-    function displayAllCars() {
-      displayCars(cars);
-      attachEventListeners();
-    }
-  
-    function displayCars(carsToDisplay) {
-      const  carList = $('#car-list');
-      carList.empty();
-  
-      carsToDisplay.forEach(function (car) {
-        const carItem = `
-      <li class="car-item" data-id="${car.id}">
-        <img src="${car.image}" alt="Car image" class="car-image">
-        <div class="car-info">
-          <h3>${car.make} ${car.model}</h3>
-          <p>Price: $${car.price}</p>
-          <button class="add-to-cart" data-id="${car.id}">Add to Cart</button>
-        </div>
-      </li>
-    `;
+var carList = [
+  {
+    id: "1",
+    make: "Mercedes Benz",
+    model: "C-Class (W205:2015)",
+    price: 45000,
+    image: "photos/C-class.jpg",
+  },
+  {
+    id: "2",
+    make: "BMW",
+    model: "3Series (G28)",
+    price: 42000,
+    image: "photos/BMW 3 series.jpeg",
+  },
+  {
+    id: "3",
+    make: "Volkswagen",
+    model: "Golf 8",
+    price: 28000,
+    image: "photos/VW Golf 8.jpg",
+  },
+  {
+    id: "4",
+    make: "Ford",
+    model: "Fusion",
+    price: 32000,
+    image: "photos/Ford Fusion.jpg",
+  },
+  {
+    id: "5",
+    make: "Toyota",
+    model: "Camry",
+    price: 35000,
+    image: "photos/Toyota Camry.png",
+  },
+  {
+    id: "6",
+    make: "Tesla",
+    model: "Model 3",
+    price: 52000,
+    image: "photos/Tesla Model 3.jpg",
+  },
+  {
+    id: "7",
+    make: "Nissan",
+    model: "Leaf",
+    price: 29000,
+    image: "photos/Nissan Leaf.jpg",
+  },
+  {
+    id: "8",
+    make: "Audi",
+    model: "A4",
+    price: 47000,
+    image: "photos/Audi-A4.jpg",
+  },
+  {
+    id: "9",
+    make: "Mercedes Benz",
+    model: "E-Class",
+    price: 55000,
+    image: "photos/Mercedes E-Class.jpg",
+  },
+  {
+    id: "10",
+    make: "Chevrolet",
+    model: "Bolt",
+    price: 32000,
+    image: "photos/Chevrolet-Bolt.jpg",
+  },
+  {
+    id: "11",
+    make: "BMW",
+    model: "i3",
+    price: 49000,
+    image: "photos/BMW i3.jpg",
+  },
+];
 
-    carList.append(carItem);
-      });
-  
-      carList.find('li').click(function () {
-        $(this).toggleClass('selected');
-      });
-    }
-  
-    function updateCartDisplay() {
-      var cartList = $('#cart-list');
-      cartList.empty();
-    
-      var total = 0;
-    
-      cars.forEach(function (car, index) {
-        var cartItem = cartList.find(`[data-id="${car.id}"]`);
-        var quantity = 0;
-    
-        if (cartItem.length > 0) {
-          quantity = parseInt(cartItem.find('.quantity').text()) || 1;
-          cartItem.find('.quantity').text(quantity);
-        } else {
-          quantity = 1;
-          cartItem = createCartItem(car);
-          cartList.append(cartItem);
-        }
-    
-        // Update individual car price
-        var individualCarPrice = car.price * quantity;
-        cars[index].price = individualCarPrice; // Assuming 'cars' is a global array
-    
-        // Update total price
-        total += individualCarPrice;
-      });
-    
-      $('#total').text(`Total: $${total.toFixed(2)}`);
-    }
-    
-    
-    function createCartItem(car) {
-      var listItem = $(`<li data-id="${car.id}">`);
-      listItem.append(`<p>${car.make} ${car.model}</p>`);
-      listItem.append(`<p class="car-price">$${car.price}</p>`);
-      listItem.append('<p class="quantity">1</p>');
-      listItem.append(`<button class="remove-from-cart" data-id="${car.id}">Remove</button>`);
-      return listItem;
-    }
-    
-    
-    function attachEventListeners() {
-      $('#car-list').on('click', '.add-to-cart', function () {
-        var carId = $(this).data('id');
-        var selectedCar = cars.find(function (car) {
-          return car.id === carId;
-        });
-  
-        updateCartDisplay();
-      });
-  
-      $('#cart-list').on('click', '.remove-from-cart', function () {
-        var carId = $(this).data('id');
-        $('[data-id="' + carId + '"]').remove();
-        updateCartDisplay();
-      });
-  
-      $('#make-filter, #price-filter-min, #price-filter-max').change(function () {
-        var makeFilter = $('#make-filter').val();
-        var minPrice = $('#price-filter-min').val();
-        var maxPrice = $('#price-filter-max').val();
-        filterCars(makeFilter, minPrice, maxPrice);
-      });
-    }
-  
-    $('#checkout').click(function () {
-      $('#sale').show();
-      $('.add-to-cart').prop('disabled', true);
-    });
-  
-    $('#sale').click(function () {
+// Function to add a new car to the list
+function addCar(make, model, price, image) {
+  var newCar = {
+    id: String(carList.length + 1),
+    make: make,
+    model: model,
+    price: price,
+    image: image,
+  };
+  carList.push(newCar);
+}
+
+// Form submission event handler
+$("#car-form").on("submit", function (event) {
+  event.preventDefault();
+  var make = $("#make-input").val();
+  var model = $("#model-input").val();
+  var price = parseInt($("#price-input").val());
+  var imageFile = $("#image-input")[0].files[0];
+  var image = "path/to/default-image.jpg"; // Default image in case no file is selected
+
+  if (imageFile) {
+    var reader = new FileReader();
+    reader.onload = function (event) {
+      image = event.target.result;
+      addCar(make, model, price, image);
+      renderCars(carList);
+      $("#car-form")[0].reset();
+    };
+
+    reader.readAsDataURL(imageFile);
+  } else {
+    addCar(make, model, price, image);
+    renderCars(carList);
+    $("#car-form")[0].reset();
+  }
+});
+
+// Function to render the car list in the UI
+function renderCars(carList) {
+  var carListContainer = $("#car-list");
+  carListContainer.empty();
+
+  carList.forEach(function (car) {
+    carListContainer.append(
+      `<li data-id="${car.id}">
+        <div class="car-item">
+          <img class="car-image" src="${car.image}" alt="${car.make} ${
+        car.model
+      }">
+          <div class="car-info">
+            <span class="make">${car.make}</span>
+            <span class="model">${car.model}</span>
+            <span class="price">Price: $${car.price.toLocaleString()}</span>
+            <button class="add-to-cart">Add to Cart</button>
+          </div>
+        </div>
+      </li>`
+    );
+  });
+}
+
+// Function to filter cars based on user input
+function filterCars() {
+  var make = $("#make-filter").val().toLowerCase().replace(/ /g, "-");
+  var minPrice = parseInt($("#price-filter-min").val()) || 0;
+  var maxPrice = parseInt($("#price-filter-max").val()) || Infinity;
+
+  $("#car-list li").each(function () {
+    var carMake = $(this).find(".make").text().toLowerCase().replace(/ /g, "-");
+    var carPrice = parseInt($(this).find(".price").text().replace("Price: $", "").replace(/,/g, ""));
+
+    if (
+      (make === "all" || carMake === make) &&
+      carPrice >= minPrice &&
+      carPrice <= maxPrice
+    ) {
+      $(this).show();
+    } else {
       $(this).hide();
-  
-      var selectedCarIds = [];
-      $('#cart-list li').each(function () {
-        var carId = $(this).data('id');
-        selectedCarIds.push(carId);
-      });
-  
-      cars = cars.filter(function (car) {
-        return !selectedCarIds.includes(car.id);
-      });
-  
-      displayAllCars();
-      updateCartDisplay();
-    });
-  
-    function filterCars(makeFilter, minPrice, maxPrice) {
-      var filteredCars = cars.filter(function (car) {
-        return (
-          (makeFilter === 'all' || car.make === makeFilter) &&
-          (!minPrice || car.price >= minPrice) &&
-          (!maxPrice || car.price <= maxPrice)
-        );
-      });
-  
-      displayCars(filteredCars);
     }
-  
-    displayAllCars();
+  });
+}
+
+
+// Function to update the shopping cart
+function updateCart() {
+  var total = 0;
+  $("#cart-list").empty();
+
+  $("#car-list li.selected").each(function () {
+    var make = $(this).find(".make").text();
+    var model = $(this).find(".model").text();
+    var price = parseInt(
+      $(this).find(".price").text().replace("Price: $", "").replace(/,/g, "")
+    );
+
+    total += price;
+
+    $("#cart-list").append(
+      `<li data-id="${$(this).data("id")}">
+        <span class="make">${make}</span>
+        <span class="model">${model}</span>
+        <span class="price">Price: $${price.toLocaleString()}</span>
+        <button class="remove-from-cart">Remove</button>
+      </li>`
+    );
+  });
+
+  $("#total").text("Total: $" + total.toLocaleString());
+}
+
+// Document ready event
+$(document).ready(function () {
+  renderCars(carList);
+
+  // Event handlers for filtering and interacting with the car list
+  $("#make-filter, #price-filter-min, #price-filter-max").change(function () {
+    filterCars();
+  });
+
+  $('#car-list').on('click', 'li', function () {
+    $(this).toggleClass('selected');
   });
   
+  $('#car-list').on('click', '.add-to-cart', function () {
+    var car = $(this).parent();
+    car.toggleClass('selected');
+    updateCart();
+  });
+
+  $("#cart-list").on("click", ".remove-from-cart", function () {
+    var car = $(this).parent();
+    var carId = car.data("id");
+    $('#car-list li[data-id="' + carId + '"]').removeClass("selected");
+    car.remove();
+    updateCart();
+  });
+
+  // Checkout and sale event handlers
+  $("#checkout").on("click", function () {
+    var cartItems = $("#cart-list li");
+    if (cartItems.length === 0) {
+      alert("Your shopping cart is empty. Please add items before checkout.");
+    } else {
+      $("#sale").show();
+      $(".add-to-cart").prop("disabled", true);
+    }
+  });
+
+  $("#sale").on("click", function () {
+    $(this).hide();
+
+    // Remove selected items from the car list
+    var selectedCarIds = $("#cart-list li")
+      .map(function () {
+        return $(this).data("id");
+      })
+      .get();
+    carList = carList.filter(function (car) {
+      return !selectedCarIds.includes(car.id);
+    });
+
+    renderCars(carList);
+    updateCart();
+  });
+});
